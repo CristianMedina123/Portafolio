793c22f03dc05dbf9d9249ae08b8437e8b33cf19
CristianMedinaGlez123 <cris11medinaglez@gmail.com>
2021-11-20 10:30:52 -0700

    Subir mi portafolio

---
+++ config/index.js
@@ -0,0 +1,114 @@
+module.exports = {
+  //-- SITE SETTINGS -----
+  author: "@konstantinmuenster",
+  siteTitle: "Cristian Medina Glez",
+  siteShortTitle: "Cristian", // Used as logo text in header, footer, and splash screen
+  siteDescription: "Cristian Medina González web portfolio.",
+  siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Cristian Medina Glez", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  useCookieBar: false, // If you use Google Analytics and want to be GDPR-compliant, set it to true
+  googleAnalyticsTrackingId: "", // e.g. UA-XXXXXX-X
+
+  // -- THEME SETTINGS -----
+  colors: {
+    lightTheme: {
+      primary: "#000000",
+      secondary: "#FFF4D9",
+      tertiary: "#F2F2F2",
+      text: "#000000",
+      subtext: "#555555",
+      background: "#FFFFFF",
+      card: "#FFFFFF",
+      scrollBar: "rgba(0, 0, 0, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+    darkTheme: {
+      primary: "#FAFAFA",
+      secondary: "#2A2926",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#121212",
+      card: "#1C1C1C",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+  },
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  //-- ARTICLES SECTION SETTINGS -----
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@cris11medinaglez
+  mediumRssFeed:
+    //"https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40cris11medinaglez",
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40cris11medinaglez",
+  // rssFeed:"https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+  shownArticles: 2,
+
+  //-- SOCIAL MEDIA SETTINGS -----
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance, E-Mail
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://mx.linkedin.com/in/cristian-medina-1827931b4",
+    },
+    {
+      name: "ORCID",
+      url: "https://orcid.org/0000-0001-8826-6986",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/CristianMedina123/",
+    },
+    {
+      name: "Mail",
+      url: "https://www.behance.net/konstanmnster",
+    },
+    // {
+    //   name: "Mail",
+    //   url: "mailto:mail@konstantin.digital",
+    // },
+  ],
+
+  //-- NAVIGATION SETTINGS -----
+  navLinks: {
+    menu: [
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Features",
+        url: "/#projects",
+      },
+      {
+        name: "Contact",
+        url: "/#contact",
+      },
+    ],
+    button: {
+      useFileName: true,
+      name: "Resume",
+      fileName: "cv_CristianMedinaGonzalez.pdf", // the file has to be placed inside the static folder at the root level
+      url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+    },
+  },
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
+}
